e056a76bb2a3e2bb147a89d695bce00708544482
Sakhaika <sakha00119@gmail.com>
2026-01-28 23:36:46 +0700

    belajar

---
+++ contoh.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello, World!" << endl;
+    int a = 10;
+    int b = 20;
+    cout << "A = 10" << endl;
+    cout << "B = 20" << endl;
+    cout << "Jadi A tambah B menjadi 30";
+    return 0;
+}
